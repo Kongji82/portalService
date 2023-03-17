4df61f19d4b2a4e364cb5b818659bc66eb04c376
kongji <kongji4092@gmail.com>
2023-03-17 18:24:41 +0900

    포탈서비스개발론 3주차

---
+++ userdao/src/test/java/kr/ac/jejunu/user/UserDaoTest.java
@@ -0,0 +1,23 @@
+package kr.ac.jejunu.user;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class UserDaoTest {
+    @Test
+    public void get() throws SQLException, ClassNotFoundException {
+        Long id = 1l;
+        String name = "kong";
+        String password = "123";
+        UserDao userDao = new UserDao();
+        User user = userDao.findById(id);
+        assertThat(user.getId(), is(id));
+        assertThat(user.getName(), is(name));
+        assertThat(user.getPassword(), is(password));
+    }
+}
