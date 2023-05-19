ae23052ba9e3ee517fb075736d617f85ce9ec1c2
kongji <kongji4092@gmail.com>
2023-05-19 17:31:42 +0900

    Handler Exception Resolver

--- userdao/src/main/java/kr/ac/jejunu/user/UserController.java
+++ userdao/src/main/java/kr/ac/jejunu/user/UserController.java
@@ -2,18 +2,32 @@ package kr.ac.jejunu.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping
 @RequiredArgsConstructor
 public class UserController {
     private final UserDao userDao;
 
-    @RequestMapping
+    @RequestMapping(path = "user", produces = "text/html")
     public User getUser(@RequestParam long id){
         System.out.println("*********RequestMapping**********");
         return userDao.findById(id);
+    }
+
+    @RequestMapping("/exception")
+    public void exception(){
+        throw new RuntimeException("어이쿠");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView error(Exception e){
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("e", e);
+        return modelAndView;
     }
 }
