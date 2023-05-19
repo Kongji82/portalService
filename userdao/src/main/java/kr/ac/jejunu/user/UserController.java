9e39a78249ec04614b33ac3afa8c77886a37e62e
kongji <kongji4092@gmail.com>
2023-05-19 17:57:13 +0900

    file upload

--- userdao/src/main/java/kr/ac/jejunu/user/UserController.java
+++ userdao/src/main/java/kr/ac/jejunu/user/UserController.java
@@ -1,11 +1,21 @@
 package kr.ac.jejunu.user;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @Controller
 @RequestMapping
@@ -29,5 +39,24 @@ public class UserController {
         ModelAndView modelAndView = new ModelAndView("error");
         modelAndView.addObject("e", e);
         return modelAndView;
+    }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.GET)
+    public void upload(){
+
+    }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public ModelAndView upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
+        String path = request.getServletContext().getRealPath("/") +
+                "/WEB-INF/static/" + file.getOriginalFilename();
+        FileOutputStream fileOutputStream = new FileOutputStream(path);
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+        bufferedOutputStream.write(file.getBytes());
+        bufferedOutputStream.close();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("url", "images/" + file.getOriginalFilename());
+        return modelAndView;
+
     }
 }
