d06a90e34033996fe186dc1b157a23336c1b9d2c
kongji <kongji4092@gmail.com>
2023-05-19 16:54:14 +0900

    Request Mapping Handler Adapter

---
+++ userdao/src/main/java/kr/ac/jejunu/user/UserController.java
@@ -0,0 +1,19 @@
+package kr.ac.jejunu.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserDao userDao;
+
+    @RequestMapping
+    public User getUser(@RequestParam long id){
+        System.out.println("*********RequestMapping**********");
+        return userDao.findById(id);
+    }
+}
